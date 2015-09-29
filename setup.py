08c43f46b4a8f41fc335ae91a42ce535aa57dfe7
Allan Lei <allanlei@helveticode.com>
2015-09-29 18:21:25 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zipstream',
-    version='1.1.0',
+    version='1.1.1',
     description='Zipfile generator',
     author='Allan Lei',
     author_email='allanlei@helveticode.com',
--- zipstream/__init__.py
+++ zipstream/__init__.py
@@ -6,7 +6,7 @@ Derived directly from zipfile.py
 """
 from __future__ import unicode_literals, print_function, with_statement
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 import os
 import sys
