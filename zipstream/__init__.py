5476a1849190f248dd8532acab2803182ef2c1a4
Allan Lei <allanlei@helveticode.com>
2016-05-06 21:02:18 +0800

    Bump version

--- zipstream/__init__.py
+++ zipstream/__init__.py
@@ -6,7 +6,7 @@ Derived directly from zipfile.py
 """
 from __future__ import unicode_literals, print_function, with_statement
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 import os
 import sys
