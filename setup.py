dc204b37434fcd4567ba7f5ededa55dcdac7bdc9
ch0k0bn <aquint@quarkslab.com>
2018-12-28 09:49:09 +0100

    Release temporary v1.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zipstream',
-    version='1.1.4',
+    version='1.1.5',
     description='Zipfile generator',
     author='Allan Lei',
     author_email='allanlei@helveticode.com',
